ba7aeaf021b170c91474b40811f2d3985a0af469
Gonzalo Arramon <gonzaarramon7@gmail.com>
2024-04-29 20:45:49 -0300

    clase

---
+++ Cursos programacion/Curso FS/Clase 3/Clase3.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readlineSync = require("readline-Sync");
+var alturaPersona = readlineSync.question("ingrese su altura: ");
+console.log(alturaPersona);
---
+++ Cursos programacion/Curso FS/Clase 3/Clase3.ts
@@ -0,0 +1,3 @@
+import * as readlineSync from "readline-Sync";
+let alturaPersona = readlineSync.question("ingrese su altura: ");
+console.log(alturaPersona);
