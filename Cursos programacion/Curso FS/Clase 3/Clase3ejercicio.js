9441e8a01772b270cc4937727a98fca617b36d1d
Gonzalo Arramon <gonzaarramon7@gmail.com>
2024-05-03 12:02:29 -0300

    a

--- Cursos programacion/Curso FS/Clase 3/Clase3ejercicio.js
+++ Cursos programacion/Curso FS/Clase 3/Clase3ejercicio.js
@@ -1,12 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var readlineSync = require("readline-sync");
-var input = readlineSync.question("Ingrese el numero a evaluar: ");
-var numero = parseFloat(input);
-if (numero === 0) {
+var numero = readlineSync.question("Ingrese el numero a evaluar: ");
+if (numero == 0) {
     console.log("el numero es cero");
 }
-else if (numero % 2 === 0) {
+else if (numero % 2 == 0) {
     console.log("El numero es par.");
 }
 else {
--- Cursos programacion/Curso FS/Clase 3/Clase3ejercicio.ts
+++ Cursos programacion/Curso FS/Clase 3/Clase3ejercicio.ts
@@ -1,9 +1,8 @@
 import * as readlineSync from 'readline-sync';
-    const input = readlineSync.question("Ingrese el numero a evaluar: ")
-    const numero = parseFloat(input);
-    if (numero === 0){
+let numero : number = readlineSync.question("Ingrese el numero a evaluar: ");
+    if (numero == 0){
         console.log("el numero es cero");
-    } else if (numero % 2 === 0){
+    } else if (numero % 2 == 0){
         console.log("El numero es par.")
     } else {
         console.log("El numero es impar")
