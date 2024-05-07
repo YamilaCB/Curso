afa580552f7664ce081d5a7893abfef56f0c1ada
Gonzalo Arramon <gonzaarramon7@gmail.com>
2024-05-06 21:45:03 -0300

    a

---
+++ Cursos programacion/Curso FS/Clase 5/Clase5.ts
@@ -0,0 +1,13 @@
+/* 
+Modularizacion y metodos:
+cuando devemos repetir codigo, creamos una funcion que despues puede ser invocada, por ejemplo:
+function dibujarGuion   (creamos el codigo para la funcion)
+y luego para llamar la funcion:
+dibujarGuion ();
+
+la funcion se puede declarar de 3 formas:
+como sentencia, como valor de una variable, como arrow function
+
+la funcion puede tener un valor modificable que se define dentro de esta cuando la declaramos, por ejemplo:
+dibujarGuion (30);
+*/
