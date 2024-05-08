1707e833cd9393575ec406dab0aa4dcce9c9eaa7
Gonzalo Arramon <gonzaarramon7@gmail.com>
2024-05-08 18:51:14 -0300

    a

---
+++ Cursos programacion/Ref/Python/Operadores/Op-comparacion.py
@@ -0,0 +1,9 @@
+'''
+sirven para comparar 2 valores y va a devolver true or false
+==     igual a
+!=     distinto a
+<      menor que
+<=     menor o igual que
+>      mayor que
+>=     mayor o igual que
+'''
