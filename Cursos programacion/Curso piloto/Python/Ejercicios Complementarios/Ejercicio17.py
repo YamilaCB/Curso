41ba58bc26caa0de2b1c373ea3c754242dba72f6
Gonzalo Arramon <gonzaarramon7@gmail.com>
2024-05-15 11:48:23 -0300

    a

---
+++ Cursos programacion/Curso piloto/Python/Ejercicios Complementarios/Ejercicio17.py
@@ -0,0 +1,9 @@
+print("------------------------------------------")
+a= int(input("Ingrese el primer numero: "))
+b= int(input("Ingrese el segundo numero: "))
+c= int(input("Ingrese el tercer numero: "))
+if a+b==c:
+    print("la suma del primer numero y el segundo es igual al tercero")
+else:
+    print("la suma del primer y segundo numero no es igual al tercero")
+print("------------------------------------------")
