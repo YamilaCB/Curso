a5633e4a9da6c3643ff1a462c4673e7d5df804df
Gonzalo Arramon <gonzaarramon7@gmail.com>
2024-05-28 17:15:38 -0300

    a

---
+++ Cursos programacion/Curso FS/Ejercicios/ClaveEureka.js
@@ -0,0 +1,9 @@
+var contador = 1;
+for (contador >= 100; contador++;) {
+    if (contador % 2 == 0)
+        console.log("El numero ".concat(contador, " es divisible por 2."));
+    if (contador % 3 == 0)
+        console.log("El numero ".concat(contador, " es divisible por 3."));
+    else
+        console.log("el numero no es divisible por 2 o 3.");
+}
---
+++ Cursos programacion/Curso FS/Ejercicios/ClaveEureka.ts
@@ -0,0 +1,11 @@
+let contador = 1
+for (contador >= 100; contador++;){
+    if (contador % 2 == 0)
+        console.log(`El numero ${contador} es divisible por 2.`)
+    if (contador % 3 == 0)
+        console.log(`El numero ${contador} es divisible por 3.`)
+else 
+    console.log ("el numero no es divisible por 2 o 3.")
+}
+
+
