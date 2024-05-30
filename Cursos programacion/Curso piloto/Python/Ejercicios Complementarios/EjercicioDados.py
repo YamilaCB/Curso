5968e6f7c7d28f2596f64a8b9d92f056db6a36b4
Gonzalo Arramon <gonzaarramon7@gmail.com>
2024-05-30 09:27:34 -0300

    Update EjercicioDados.py

--- Cursos programacion/Curso piloto/Python/Ejercicios Complementarios/EjercicioDados.py
+++ Cursos programacion/Curso piloto/Python/Ejercicios Complementarios/EjercicioDados.py
@@ -3,7 +3,7 @@ def calcular_probabilidad(dados):
     #probabilidad = (1/6 ** dados)
     return probabilidad
 dados = int(input("Ingrese la cantidad de dados: "))
-
+#probabilidad_6= round(calcular_probabilidad(dados), 2)
 probabilidad_6= calcular_probabilidad(dados)
 #print(f"la probabilidad de salir el numero 6 en {dados} dados es de: {probabilidad_6: .10f}")
 print(f"la probabilidad de salir el numero 6 en {dados} dados es de: 1 entre {probabilidad_6}")
