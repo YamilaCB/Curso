e52cd308e52eb0f6a933b21905a12737655fa324
Gonzalo Arramon <gonzaarramon7@gmail.com>
2024-05-30 08:52:04 -0300

    a

---
+++ Cursos programacion/Curso piloto/Python/Ejercicios Complementarios/EjercicioDados.py
@@ -0,0 +1,10 @@
+def calcular_probabilidad(n):
+    probabilidad = (1/6 ** dados)
+    return probabilidad
+dados = int(input("Ingrese la cantidad de dados: "))
+
+probabilidad_6 = calcular_probabilidad(n)
+print(f"la probabilidad de salir el numero 6 en ${dados} es de ${probabilidad_6}")
+
+
+
